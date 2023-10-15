aacf2042a6ff9b3e413c794b51b966dcfaaec410
POONAM RATHOD <138007461+rathodpoonam@users.noreply.github.com>
2023-10-15 21:10:55 +0530

    All file and images added .

---
+++ index.js
@@ -0,0 +1,16 @@
+// Leaflet Map
+
+// var map = L.map('map').setView([53.342686, -6.267118], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
+
+var map = L.map('map').setView([21.1458, 79.0882], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
